063e0af3ddecd92e0367e3b18ac2be0ee16a65e6
Siyamed Sinir <siyamed@google.com>
2018-04-11 23:58:25 -0700

    Update emoji diff check script
    Test: Ran the tool on new font
    Bug: 77148691
    Change-Id: I8d1bfd17698cc4e1653f31760f4b8e4c73f20799

--- scripts/color_emoji_image_diff.py
+++ scripts/color_emoji_image_diff.py
@@ -15,10 +15,88 @@
 # limitations under the License.
 
 import contextlib
+import itertools
 import os
 import sys
 
 from fontTools import ttLib
+
+def to_hex_str(value):
+    """Converts given int value to hex without the 0x prefix"""
+    return format(value, 'X')
+
+def codepoint_to_string(codepoints):
+    """Converts a list of codepoints into a string separated with space."""
+    return '_'.join([to_hex_str(x) for x in codepoints])
+
+def read_cmap12(ttf, glyph_to_codepoint_map, codepoint_map):
+    cmap = ttf['cmap']
+    for table in cmap.tables:
+        if table.format == 12 and table.platformID == 3 and table.platEncID == 10:
+            for codepoint, glyph_name in table.cmap.iteritems():
+                glyph_to_codepoint_map[glyph_name] = codepoint
+                codepoint_map[codepoint_to_string([codepoint])] = glyph_name
+                # self.update_emoji_data([codepoint], glyph_name)
+            return table
+    raise ValueError("Font doesn't contain cmap with format:12, platformID:3 and platEncID:10")
+
+def read_gsub(ttf, glyph_to_codepoint_map, codepoint_map):
+    gsub = ttf['GSUB']
+    ligature_subtables = []
+    context_subtables = []
+    # this code is font dependent, implementing all gsub rules is out of scope of EmojiCompat
+    # and would be expensive with little value
+    for lookup in gsub.table.LookupList.Lookup:
+        for subtable in lookup.SubTable:
+            if subtable.LookupType == 5:
+                context_subtables.append(subtable)
+            elif subtable.LookupType == 4:
+                ligature_subtables.append(subtable)
+
+    for subtable in context_subtables:
+        add_gsub_context_subtable(subtable, gsub.table.LookupList, glyph_to_codepoint_map, codepoint_map)
+
+    for subtable in ligature_subtables:
+        add_gsub_ligature_subtable(subtable, glyph_to_codepoint_map, codepoint_map)
+
+def add_gsub_context_subtable(subtable, lookup_list, glyph_to_codepoint_map, codepoint_map):
+    for sub_class_set in subtable.SubClassSet:
+        if sub_class_set:
+            for sub_class_rule in sub_class_set.SubClassRule:
+                subs_list = len(sub_class_rule.SubstLookupRecord) * [None]
+                for record in sub_class_rule.SubstLookupRecord:
+                    subs_list[record.SequenceIndex] = get_substitutions(lookup_list,
+                                                                        record.LookupListIndex)
+                combinations = list(itertools.product(*subs_list))
+                for seq in combinations:
+                    glyph_names = [x["input"] for x in seq]
+                    codepoints = [glyph_to_codepoint_map[x] for x in glyph_names]
+                    outputs = [x["output"] for x in seq if x["output"]]
+                    nonempty_outputs = filter(lambda x: x.strip() , outputs)
+                    if len(nonempty_outputs) == 0:
+                        print("Warning: no output glyph is set for " + str(glyph_names))
+                        continue
+                    elif len(nonempty_outputs) > 1:
+                        print(
+                            "Warning: multiple glyph is set for "
+                                + str(glyph_names) + ", will use the first one")
+
+                    glyph = nonempty_outputs[0]
+                    codepoint_map[codepoint_to_string(codepoints)] = glyph
+
+def get_substitutions(lookup_list, index):
+    result = []
+    for x in lookup_list.Lookup[index].SubTable:
+        for input, output in x.mapping.iteritems():
+            result.append({"input": input, "output": output})
+    return result
+
+def add_gsub_ligature_subtable(subtable, glyph_to_codepoint_map, codepoint_map):
+    for name, ligatures in subtable.ligatures.iteritems():
+        for ligature in ligatures:
+            glyph_names = [name] + ligature.Component
+            codepoints = [glyph_to_codepoint_map[x] for x in glyph_names]
+            codepoint_map[codepoint_to_string(codepoints)] = ligature.LigGlyph
 
 def read_cbdt(ttf):
   cbdt = ttf['CBDT']
@@ -30,26 +108,46 @@ def read_cbdt(ttf):
   return glyph_to_image
 
 def main(argv):
+  codepoint_map_1 = {}
+  codepoint_map_2 = {}
+  glyph_to_codepoint_map_1 = {}
+  glyph_to_codepoint_map_2 = {}
+
   with contextlib.closing(ttLib.TTFont(argv[1])) as ttf:
     font1_cbdt = read_cbdt(ttf)
+    read_cmap12(ttf, glyph_to_codepoint_map_1, codepoint_map_1)
+    read_gsub(ttf, glyph_to_codepoint_map_1, codepoint_map_1)
   with contextlib.closing(ttLib.TTFont(argv[2])) as ttf:
     font2_cbdt = read_cbdt(ttf)
+    read_cmap12(ttf, glyph_to_codepoint_map_2, codepoint_map_2)
+    read_gsub(ttf, glyph_to_codepoint_map_2, codepoint_map_2)
 
   glyphs1 = set(font1_cbdt.keys())
   glyphs2 = set(font2_cbdt.keys())
-  if glyphs1 != glyphs2:
-    print "Glyph set has changed: : %s" % (glyphs1 ^ glyphs2)
 
-  for key in font1_cbdt.keys():
-    image1 = font1_cbdt[key]
-    image2 = font2_cbdt[key]
+  codepoints_set1 = set(codepoint_map_1.keys())
+  codepoints_set2 = set(codepoint_map_2.keys())
+
+  if codepoints_set1 != codepoints_set2:
+    print "Codepoints set has changed: : %s" % (codepoints_set1 ^ codepoints_set2)
+
+  all_codepoints = set(codepoint_map_1.keys()).union(codepoint_map_2.keys())
+
+  for key in all_codepoints:
+    glyph1 = codepoint_map_1[key] if key in codepoint_map_1 else None
+    glyph2 = codepoint_map_2[key] if key in codepoint_map_2 else None
+
+    image1 = font1_cbdt[glyph1] if glyph1 and glyph1 in font1_cbdt else None
+    image2 = font2_cbdt[glyph2] if glyph2 and glyph2 in font2_cbdt else None
 
     if image1 != image2:
       print 'Glyph %s has different image' % key
-      with open(os.path.join(argv[3], '%s_old.png' % key), 'w') as f:
-        f.write(image1)
-      with open(os.path.join(argv[3], '%s_new.png' % key), 'w') as f:
-        f.write(image2)
+      if image1:
+        with open(os.path.join(argv[3], '%s_old.png' % key), 'w') as f:
+            f.write(image1)
+      if image2:
+        with open(os.path.join(argv[3], '%s_new.png' % key), 'w') as f:
+            f.write(image2)
 
 if __name__ == '__main__':
   main(sys.argv)
